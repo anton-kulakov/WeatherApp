0be14bad28fe93eeba4aa003de153d1557525f5c
anton kulakov <kulakov-akv@yandex.ru>
2025-02-12 10:42:19 +0300

    refactor: remove unnecessary code

--- src/main/java/dev/anton_kulakov/validator/RedirectUrlValidator.java
+++ src/main/java/dev/anton_kulakov/validator/RedirectUrlValidator.java
@@ -9,15 +9,9 @@ public class RedirectUrlValidator {
     private static final Set<String> ALLOWED_REDIRECTS = Set.of("/sign-in", "/sign-up", "/index",  "/search");
 
     public String getValidRedirectToUrl(String redirectToUrl) {
-        String result = "/index";
-
-        if (isRedirectUrlValid(redirectToUrl)) {
-            result = redirectToUrl;
-        }
-
-        return result;
+        return isRedirectUrlValid(redirectToUrl) ? redirectToUrl : "/index";
     }
     private boolean isRedirectUrlValid(String url) {
-        return ALLOWED_REDIRECTS.contains(url);
+        return url!= null && ALLOWED_REDIRECTS.contains(url);
     }
 }
